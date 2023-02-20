8aba28ed4bcd819c8bddfe9a595b62d38a72e60a
WalterWoshid <wotschel.valentin@googlemail.com>
2023-02-20 18:43:29 +0100

    Added base exception

---
+++ src/Exception/CodeTransformerException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Okapi\CodeTransformer\Exception;
+
+use RuntimeException;
+
+/**
+ * # Code Transformer Exception
+ *
+ * Base exception for all Code Transformer exceptions.
+ */
+abstract class CodeTransformerException extends RuntimeException
+{
+}
