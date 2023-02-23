3eb36fa63411c21bbc072baf615dbc0900ebf5d7
WalterWoshid <wotschel.valentin@googlemail.com>
2023-02-23 04:03:04 +0100

    Added util classes

---
+++ src/Util/StringMutator.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Okapi\CodeTransformer\Util;
+
+/**
+ * A utility class for mutating strings.
+ *
+ * This class is used to mutate strings in a way that preserves the positions of
+ * edits. This is useful for transforming code, where edits to the code may
+ * change the positions of other edits.
+ */
+class StringMutator
+{
+    /**
+     * The string to mutate.
+     *
+     * @var string
+     */
+    private string $string;
+
+    /**
+     * The edits to apply to the string.
+     *
+     * @var array{int, int, string}[] Start position, length, replacement
+     */
+    public array $edits = [];
+
+    /**
+     * StringMutator constructor.
+     *
+     * @param string $string
+     */
+    public function __construct(string $string)
+    {
+        $this->string = $string;
+    }
+
+    /**
+     * Add an edit to the string.
+     *
+     * @param int    $start
+     * @param int    $length
+     * @param string $replacement
+     *
+     * @return $this
+     */
+    public function edit(int $start, int $length, string $replacement): self
+    {
+        $this->edits[] = [$start, $length, $replacement];
+        return $this;
+    }
+
+    /**
+     * Get the mutated string.
+     *
+     * @return string
+     */
+    public function getMutatedString(): string
+    {
+        // Sort the edits by start position.
+        usort($this->edits, function ($a, $b) {
+            return $a[0] <=> $b[0];
+        });
+
+        // Apply the edits.
+        $result = $this->string;
+        $offset = 0;
+        foreach ($this->edits as $edit) {
+            [$start, $length, $replacement] = $edit;
+            $start += $offset;
+            $result = substr_replace($result, $replacement, $start, $length);
+            $offset += strlen($replacement) - $length;
+        }
+        return $result;
+    }
+}
